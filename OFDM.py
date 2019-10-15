cc080e03334cd3d955c03ebcf59ed9e295e51e83
Lucas Eliseu Gonçalves de Arruda <lucas.arruda@ee.ufcg.edu.br>
2019-10-15 16:20:00 -0300

    OFDM activity updates
    It now supports multiple data blocks and computes both theorical and
    practical BERes.

--- OFDM.py
+++ OFDM.py
@@ -2,9 +2,15 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 from scipy.signal import hilbert
+from scipy.stats import norm
 
-np.seed = 1
+
+np.random.seed(1)
 random.seed(1)
+
+def ErrProb(c,sigma,nBlocks, Ns):
+    return (1 - norm.cdf(c / np.sqrt(((sigma ** 2) * nBlocks * Ns))))
+
 def randsrc(dim=1, qnt=10, src=np.array([-3, -1, 1, 3])):
     aux = np.array([])
     outVar = np.empty(shape=(0,qnt),dtype=int)
@@ -21,28 +27,39 @@ def slicer(arrayMoura):
         else:
             aux.append(1)
     return aux
-# Canal h
+# Variaveis
 alfa = 0.1
+Ns = 2**15
+nBlocks = 1
+c = 1
+sigma = 0.5
+
+# Canal h
 h = np.array([1, alfa])
 mu = len(h) - 1
 
-a = randsrc(1, 8, [-1, 1])
+a = randsrc(nBlocks, Ns, [-c, c])
+aChapeu = np.zeros(np.shape(a))
 
-s = np.fft.ifft(a[0],8)
+for i in range(nBlocks):
+    s = np.fft.ifft(a[i],Ns)
+    s = np.append(s[Ns-1], s)
+    s = np.convolve(s, h)
+    s = s + np.random.normal(0, sigma, np.shape(s))
 
-s = s + np.random.normal(0, 0.5, np.shape(s))
+    s = s[1:]
+    r = np.fft.fft(s,Ns)
 
-r = np.fft.fft(s,8)
+    # posIndexes = np.nonzero(r>=0)
+    # negIndexes = np.nonzero(r<0)
 
-# posIndexes = np.nonzero(r>=0)
-# negIndexes = np.nonzero(r<0)
-
-aChapeu = slicer(r)
+    aChapeu[i] = slicer(r)
 
 #ProbErroTeor = Seria aquela função Q(sqrt(2*Eb/N0)) mas quem é N0? N0 é 2*sigma^2 porque N0 é complexo
-probErroPrat = len(np.nonzero((a - aChapeu) != 0))/8
+probErroPrat = np.shape((np.nonzero((a - aChapeu) != 0)))[1]/(Ns*nBlocks)
+errosTeor = ErrProb(c,sigma,nBlocks, Ns)
 
-print(probErroPrat, a - aChapeu)
+print(probErroPrat, errosTeor)
 
 
 
